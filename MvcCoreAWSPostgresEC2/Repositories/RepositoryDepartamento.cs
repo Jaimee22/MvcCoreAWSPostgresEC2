422cea5b5de4675c73c2bb146639cefb260fd023
Jaimee22 <alumno.410564@ies-azarquiel.es>
2024-05-14 09:23:10 +0200

    Add project files.

---
+++ MvcCoreAWSPostgresEC2/Repositories/RepositoryDepartamento.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreAWSPostgresEC2.Data;
+using MvcCoreAWSPostgresEC2.Models;
+
+namespace MvcCoreAWSPostgresEC2.Repositories
+{
+    public class RepositoryDepartamento
+    {
+        private DepartamentosContext context;
+        public RepositoryDepartamento(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int id)
+        { 
+            return await this.context.Departamentos.FirstOrDefaultAsync(x => x.IdDepartamento == id);
+        }
+
+        public async Task InsertarDepartamentoAsync(int id, string nombre, string localidad)
+        {
+            Departamento departamento = new Departamento
+            { 
+                IdDepartamento = id, Nombre = nombre, Localidad = localidad
+            };
+            this.context.Departamentos.Add(departamento);
+            await this.context.SaveChangesAsync();
+        }
+
+    }
+}
